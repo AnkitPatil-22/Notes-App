08dd62f46d3d9afe52c9992875d4e443501f5087
AnkitPatil-22 <ankitpatil2202@gmail.com>
2022-03-15 16:07:42 +0530

    Updates

---
+++ precache-manifest.764409be20cd94251538a170e7bc492b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe57a11881748bf034e5f029e45c99d",
+    "url": "/Notes-App/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/Notes-App/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "fb60fbcaf1e1bad8f2b6",
+    "url": "/Notes-App/static/js/main.7e4632d3.chunk.js"
+  },
+  {
+    "revision": "ebe664dc82ff82ec5999",
+    "url": "/Notes-App/static/js/runtime-main.6f499325.js"
+  }
+]);
