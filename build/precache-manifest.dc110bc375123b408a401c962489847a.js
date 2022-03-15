687e1189da94d1db4686da2acd0dcc4443ff2b69
AnkitPatil-22 <ankitpatil2202@gmail.com>
2022-03-15 16:06:59 +0530

    Second commit

---
+++ build/precache-manifest.dc110bc375123b408a401c962489847a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2042ff5c5c5e99ac7d09e14594bf48ae",
+    "url": "/Notes-App/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/Notes-App/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "fb60fbcaf1e1bad8f2b6",
+    "url": "/Notes-App/static/js/main.7e4632d3.chunk.js"
+  },
+  {
+    "revision": "ebe664dc82ff82ec5999",
+    "url": "/Notes-App/static/js/runtime-main.6f499325.js"
+  }
+]);
